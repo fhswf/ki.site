0b587ba75a7cfdef190e83c8c35edfe5c8485651
Christian Gawron <gawron.christian@fh-swf.de>
2023-05-08 09:34:02 +0000

    display cluster status

--- src/components/ClusterStatus.tsx
+++ src/components/ClusterStatus.tsx
@@ -2,23 +2,34 @@ import { h, Fragment } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
 
 export default function ClusterStatus() {
-    const [data, setData] = useState({ workflow_runs: [{ conclusion: "unknown" }] });
+    const [data, setData] = useState<any>();
 
-    //curl -v -H "Authorization: Bearer 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" -H "Accept: application/vnd.github+json"  'https://api.github.com/repos/fhswf/kicluster-deployments/actions/runs?event=schedule'
-    fetch('https://api.github.com/repos/fhswf/kicluster-deployments/actions/runs?event=schedule', {
-        headers: {
-            "Authorization": "Bearer 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",
-            "Accept": "application/vnd.github+json"
-        }
-    })
-        .then(response => response.json())
-        .then(data => setData(data));
+    let updateData = () => {
+        //curl -v -H "Authorization: Bearer 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" -H "Accept: application/vnd.github+json"  'https://api.github.com/repos/fhswf/kicluster-deployments/actions/runs?event=schedule'
+        fetch('https://api.github.com/repos/fhswf/kicluster-deployments/actions/runs?event=schedule', {
+            headers: {
+                "Authorization": "Bearer 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",
+                "Accept": "application/vnd.github+json"
+            }
+        })
+            .then(response => response.json())
+            .then(data => setData(data.workflow_runs.filter((run: any) => run.status === "completed")[0]));
+    }
+    //updateData();
 
-    let state = data?.workflow_runs[0]?.conclusion || "unknown";
+    useEffect(() => {
+        const interval = setInterval(updateData, 10000);
+        return () => clearInterval(interval);
+    }, [data]);
+
+    let state = data?.conclusion || "unknown";
 
     return (
-        <a href={data?.workflow_runs[0]?.html_url} className={["cluster_state", state].join(" ")} target="_empty">
-            Cluster Status
-        </a>
+        <>
+            <a href={data?.html_url} target="_empty">
+                Cluster Status:
+            </a>
+            &nbsp;<span className={["cluster_state", state].join(" ")}><div className={["cluster_state", state].join(" ")} /></span>
+        </>
     );
 }
